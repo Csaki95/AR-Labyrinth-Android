1fec2d2ac0a520d0dc38bf70692db85c169a1ea4
Csáki Gergő <33156423+Csaki95@users.noreply.github.com>
2021-01-18 00:27:55 +0100

    Pre 1.0 update
    - 3 simple new maps
    - Notification on first open
    - Direct QR link for easier usage

---
+++ Assets/Scripts/Misc/Notification.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notification : MonoBehaviour
+{
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("doNotify", 1) == 0)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    public void doNotNotify()
+    {
+        PlayerPrefs.SetInt("doNotify", 0);
+    }
+}
